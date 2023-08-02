022cd33ef32f744fa803381f725a77a8317e83a8
dmitriy <dima22022005@gmail.com>
2023-08-02 04:11:45 +0200

    Add some styles for Todo.jsx and TodoList.jsx components

--- components/Todo.jsx
+++ components/Todo.jsx
@@ -1,7 +1,32 @@
+"use client"
+
+import { MdOutlineRadioButtonUnchecked, MdOutlineRadioButtonChecked } from 'react-icons/md'
+import { IoMdClose } from 'react-icons/io'
+import { useState } from 'react'
+
 const Todo = ({ todo }) => {
+    const [isChecked, setIsChecked] = useState(false)
+
     return (
-        <div className="my-2">
-            <p className="text-slate-200">{todo}</p>
+        <div className="flex justify-between my-2 px-3">
+            { !isChecked ?
+                [   
+                    <div className="flex items-center">
+                        <MdOutlineRadioButtonUnchecked size={15} className="text-slate-200 cursor-pointer" />
+                        <p className="text-slate-200 ml-2">{todo}</p>
+                    </div>,
+                    <IoMdClose size={15} className="text-slate-200 cursor-pointer" />
+                ]
+                :
+                [   
+                    <div className="flex items-center">
+                        <MdOutlineRadioButtonChecked size={15} className="text-slate-200 cursor-pointer" />
+                        <p className="text-gray-700 ml-2">{todo}</p>
+                    </div>,
+                    <IoMdClose size={15} className="text-slate-700 ml-5 cursor-pointer" />
+                ]
+            }
+            
         </div>
     )
 }
--- components/Todolist.jsx
+++ components/Todolist.jsx
@@ -2,7 +2,7 @@ import Todo from "@/components/Todo"
 
 const TodoList = ({ todos }) => {
     return (
-        <div>
+        <div className="mt-6">
             {todos && todos.map((todo) => (
                 <Todo todo={todo} />
             ))}
