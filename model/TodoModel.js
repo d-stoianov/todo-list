b9397e8bc8366de487689a7536c5e760e6279426
dmitriy <dima22022005@gmail.com>
2023-08-04 13:22:06 +0200

    Add service.js file for work with localstorage (in future with a backend).  Also create TodoModel.js to define todo's fields

---
+++ model/TodoModel.js
@@ -0,0 +1,9 @@
+class TodoModel {
+    constructor(id, title, isChecked) {
+        this.id = id
+        this.title = title 
+        this.isChecked = isChecked
+    }
+}
+
+export default TodoModel
---
+++ service.js
@@ -0,0 +1,29 @@
+class Service {
+    async getAllTodos() {
+        return JSON.parse(localStorage.getItem("todos")) ?? [] // if no data return empty array
+    }
+
+    async createTodo(todo) {
+        const todos = await this.getAllTodos()
+        let id = 0
+        if(todos.length > 0) {
+            const firstTodo = todos[0]
+            id = firstTodo.id + 1
+        }
+        todo.id = id
+        localStorage.setItem("todos", JSON.stringify([todo, ...todos]))
+    }
+
+    async deleteTodoById(todoId) {
+        const todos = await this.getAllTodos()
+        const index = todos.findIndex((todo) => {
+            return todoId === todo.id
+        })
+        if (index >= 0) {
+            todos.splice(index, 1)
+            localStorage.setItem("todos", JSON.stringify(todos))
+        }
+    }
+}
+
+export default Service
