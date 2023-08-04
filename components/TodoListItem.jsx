1bc6199265431c71b86c5a84398f32a2e885f938
dmitriy <dima22022005@gmail.com>
2023-08-04 23:38:21 +0200

    Fix editing multiple todos

--- components/TodoListItem.jsx
+++ components/TodoListItem.jsx
@@ -5,31 +5,36 @@ import { IoMdClose } from 'react-icons/io'
 import { FiEdit } from 'react-icons/fi'
 import { useEffect, useRef, useState } from 'react'
 
-const TodoListItem = ({ onItemDelete, onItemChecked, onItemEdited, todo }) => {
+const TodoListItem = ({ onItemDelete, onItemChecked, onItemEndEditing, onItemBeginEditing, isEditing, todo }) => {
     const [isChecked, setIsChecked] = useState(todo.isChecked)
-    const [isEditing, setIsEdititng] = useState(false)
+    const [isEditingState, setIsEdititngState] = useState(isEditing)
     const titleInputRef = useRef(null)
 
     let title = ""
 
     const saveTitle = (title) => {
         todo.title = title
-        onItemEdited(todo)
+        onItemEndEditing(todo)
     }
 
     const onTitleKeyDown = (e) => {
         if (e.key === "Enter") {
-            setIsEdititng(false)
+            setIsEdititngState(false)
         }
     }
 
     useEffect(() => {
-        if (isEditing) {
+        if (isEditingState) {
             titleInputRef.current.focus()
+            onItemBeginEditing(todo)
         }
+    }, [isEditingState])
+
+    useEffect(() => {
+        setIsEdititngState(isEditing)
     }, [isEditing])
 
-    if(isEditing) {
+    if(isEditingState) {
         title = <input 
             onKeyDown={onTitleKeyDown} 
             defaultValue={todo.title} 
@@ -42,7 +47,7 @@ const TodoListItem = ({ onItemDelete, onItemChecked, onItemEdited, todo }) => {
         </input>
     } else {
         title = <p 
-            onClick={() => setIsEdititng(!isEditing)} 
+            onClick={() => setIsEdititngState(!isEditingState)} 
             className={!isChecked ? "text-slate-200 ml-2" : "text-gray-500 ml-2"}>{todo.title}
         </p>
     }
@@ -77,7 +82,7 @@ const TodoListItem = ({ onItemDelete, onItemChecked, onItemEdited, todo }) => {
                     
                 </div>
                 <div className="flex items-center gap-2">
-                    <FiEdit onClick={() => setIsEdititng(!isEditing)} size={15} className={!isChecked ? "text-slate-200 cursor-pointer" : "text-gray-700 cursor-pointer"} />
+                    <FiEdit onClick={() => setIsEdititngState(!isEditingState)} size={15} className={!isChecked ? "text-slate-200 cursor-pointer" : "text-gray-700 cursor-pointer"} />
                     <IoMdClose onClick={() => onItemDelete(todo)} size={15} className={!isChecked ? "text-slate-200 cursor-pointer" : "text-gray-700 cursor-pointer"} />
                 </div>
             </div>
--- components/Todolist.jsx
+++ components/Todolist.jsx
@@ -1,13 +1,23 @@
+"use client"
+
 import TodoListItem from "@/components/TodoListItem"
+import { useState } from "react"
 
 const TodoList = ({ onItemDelete, onItemChecked, onItemEdited, todos }) => {
+    const [currentTodo, setCurrentTodo] = useState(null)
+    const onItemBeginEditing = (todo) => {
+        setCurrentTodo(todo)
+    }
+
     return (
         <div className="mt-6">
             {todos.map((todo) => (
                 <TodoListItem 
                     onItemDelete={onItemDelete} 
                     onItemChecked={onItemChecked} 
-                    onItemEdited={onItemEdited}
+                    onItemEndEditing={onItemEdited}
+                    onItemBeginEditing={onItemBeginEditing}
+                    isEditing={currentTodo !== null && currentTodo.id === todo.id}
                     key={todo.id} 
                     todo={todo} 
                 />
