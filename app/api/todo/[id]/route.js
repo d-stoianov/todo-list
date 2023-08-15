8a91b3605a247b51908d6e2c331346eb54c61733
dmitriy <dima22022005@gmail.com>
2023-08-15 17:44:27 +0200

    Change api routes
    Each user can get/update/delete his own todos. Also add some security check

--- app/api/todo/[id]/route.js
+++ app/api/todo/[id]/route.js
@@ -1,11 +1,20 @@
 import connectToDb from "@/utils/database"
 import Todo from "@/models/todo"
+import { getServerSession } from "next-auth/next"
+import authOptions from "@/app/api/auth/[...nextauth]/authOptions"
 
 export const GET = async (request, { params }) => {
     try {
+        const session = await getServerSession(authOptions)
+        const userId = session.user.id
+
         await connectToDb()
 
         const todo = await Todo.findById(params.id)
+
+        if (!todo.creator.equals(userId)) {
+            return new Response("Access denied", { status: 403 })
+        }
 
         return new Response(JSON.stringify(todo), { status: 200 })
     } catch (error) {
@@ -17,19 +26,26 @@ export const PATCH = async (request, { params }) => {
     const { title, description, isChecked } = await request.json()
 
     try {
+        const session = await getServerSession(authOptions)
+        const userId = session.user.id
+
         await connectToDb()
         
-        const existingTodo = await Todo.findById(params.id)
+        const todo = await Todo.findById(params.id)
 
-        if (!existingTodo) {
+        if (!todo.creator.equals(userId)) {
+            return new Response("Access denied", { status: 403 })
+        }
+
+        if (!todo) {
             return new Promise("Todo not found", { status: 404 })
         }
 
-        existingTodo.title = title
-        existingTodo.description = description
-        existingTodo.isChecked = isChecked
+        todo.title = title
+        todo.description = description
+        todo.isChecked = isChecked
 
-        await existingTodo.save()
+        await todo.save()
 
         return new Response("Todo has been updated", { status: 200 })
     } catch (error) {
@@ -39,9 +55,18 @@ export const PATCH = async (request, { params }) => {
 
 export const DELETE = async (request, { params }) => {
     try {
+        const session = await getServerSession(authOptions)
+        const userId = session.user.id
+
         await connectToDb()
 
-        await Todo.findByIdAndDelete(params.id)
+        const todo = await Todo.findById(params.id)
+
+        if (!todo.creator.equals(userId)) {
+            return new Response("Access denied", { status: 403 })
+        }
+
+        await Todo.deleteOne(todo)
 
         return new Response("Todo has been deleted", { status: 200 })
     } catch (error) {
--- app/api/todo/route.js
+++ app/api/todo/route.js
@@ -1,11 +1,16 @@
 import connectToDb from "@/utils/database"
 import Todo from "@/models/todo"
+import { getServerSession } from "next-auth/next"
+import authOptions from "@/app/api/auth/[...nextauth]/authOptions"
 
 export const GET = async (request) => {
     try {
+        const session = await getServerSession(authOptions)
+        const userId = session.user.id
+
         await connectToDb()
 
-        const todos = await Todo.find({})
+        const todos = await Todo.find({creator: userId})
 
         return new Response(JSON.stringify(todos), { status: 200 })
     } catch (error) {
@@ -16,9 +21,12 @@ export const GET = async (request) => {
 export const POST = async (request) => {
     const { title, description, isChecked } = await request.json()
     try {
+        const session = await getServerSession(authOptions)
+        const userId = session.user.id
+
         await connectToDb()
 
-        const newTodo = new Todo({ title, description, isChecked })
+        const newTodo = new Todo({ title, description, isChecked, creator: userId })
         await newTodo.save()
 
         return new Response(JSON.stringify(newTodo), { status: 200 })
